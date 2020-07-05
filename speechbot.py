1ff11cc64a40c35c3c3c9946c8b7b9b93a37b7b3
HappyBoy0509 <65845452+HappyBoy0509@users.noreply.github.com>
2020-07-05 14:50:29 +0530

    Update speechbot.py

--- speechbot.py
+++ speechbot.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 file_name = "speech.ogg"
 languages = ["nl", "en-GB", "en-US", "fr", "de", "it", "ru", "cs", "pl", "es", "tr", "pt", "zh", "ar", "sv", "ja"]
 
-token = "token here" #Bot token (Example: 93181085:AAELcePZ1qabYrDiu0t1PVPuw1HI0zXzXmq)
+token = "1277112419:AAE6-iYRSo-0nmLJ0u4P_nrCjo5sfPcGI7U" #Bot token (Example: 93181085:AAELcePZ1qabYrDiu0t1PVPuw1HI0zXzXmq)
 cert = "example.crt" #SSL certificate (Can not be self-signed)
 key = "example.key"  #SSL key (Can not be self-signed)
 
